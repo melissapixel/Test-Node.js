6d5c7953ed0b9737e09214484841303cc7ae6c49
Mel <melissapixell@gmail.com>
2025-11-19 04:10:09 +0300

    Secure BD

--- bd.js
+++ bd.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'example',
-  password: '12309856', 
-  port: 5432,
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'example',
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = { pool };
