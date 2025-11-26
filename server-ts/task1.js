be975c6bc4ff8a071ee680f83149083b2fc04931
Mel <melissapixell@gmail.com>
2025-11-26 07:37:03 +0300

    first type-object

---
+++ server-ts/task1.js
@@ -0,0 +1,5 @@
+var currentUser = {
+    username: "ivan",
+    age: 25,
+    isAdmin: false
+};
---
+++ server-ts/task1.ts
@@ -0,0 +1,11 @@
+interface User {
+  username: string;
+  age: number;
+  isAdmin: boolean;
+}
+
+const currentUser: User = {
+  username: "ivan",
+  age: 25,
+  isAdmin: false
+};
